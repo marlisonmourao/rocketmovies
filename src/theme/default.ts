e2b5223c911c57a6d1136ed645f25a17ebe0c035
marlisonmourao <bentesmourao@gmail.com>
2023-03-15 00:05:24 -0400

    theme default

--- src/theme/default.ts
+++ src/theme/default.ts
@@ -2,8 +2,10 @@ export const defaultTheme = {
   white: '#FFF',
 
   background: '#1C1B1E',
+  'card-background': 'rgba(255, 133, 155, 0.05)',
 
   'gray-100': '#F4EDE8',
+  'gray-200': '#999591',
   'gray-300': '#C4C4CC',
   'gray-400': '#948F99',
   'gray-600': '#312E38',
