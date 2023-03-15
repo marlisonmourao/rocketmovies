a764357bbb9e6a6683c8ad94bf253557911ea4a2
marlisonmourao <bentesmourao@gmail.com>
2023-03-15 00:15:15 -0400

    component cardInfoFilm

---
+++ src/components/CardInfoFilm/index.tsx
@@ -0,0 +1,20 @@
+import { Container, Description, Tag, Title } from './styles'
+
+export function CardInfoFilme() {
+  return (
+    <Container>
+      <Title>Interestellar</Title>
+      <Description>
+        Pragas nas colheitas fizeram a civilização humana regredir para uma
+        sociedade agrária em futuro de data desconhecida. Cooper, ex-piloto da
+        NASA, tem uma fazenda com sua família. Murphy, a filha de dez anos de
+        Cooper, acredita que seu quarto está assombrado por um fantasma que
+        tenta se comunicar com ela. Pai e filha descobrem que o fantasma é uma
+        inteligência desconhecida que está enviando mensagens codificadas
+      </Description>
+
+      <Tag>Ficcção</Tag>
+      <Tag>series</Tag>
+    </Container>
+  )
+}
---
+++ src/components/CardInfoFilm/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 3.2rem;
+
+  background: ${({ theme }) => theme['card-background']};
+  border-radius: 1.6rem;
+  margin-bottom: 2.4rem;
+`
+
+export const Title = styled.h2`
+  font-size: 2.4rem;
+  line-height: 3.2rem;
+  font-weight: 700;
+  color: ${({ theme }) => theme['gray-100']};
+
+  margin-bottom: 0.9rem;
+`
+
+export const Description = styled.p`
+  font-size: 1.6rem;
+  line-height: 1.9rem;
+  color: ${({ theme }) => theme['gray-200']};
+  margin-bottom: 2rem;
+`
+
+export const ContainerTags = styled.div``
+
+export const Tag = styled.strong`
+  font-size: 1.2rem;
+  line-height: 1.4rem;
+  color: ${({ theme }) => theme['gray-100']};
+
+  background: ${({ theme }) => theme['gray-600']};
+  border-radius: 0.8rem;
+  padding: 0.5rem 1.6rem;
+  margin-right: 0.8rem;
+`
