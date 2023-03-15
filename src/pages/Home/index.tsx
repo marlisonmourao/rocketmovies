15c3f8d657dad30bef2960a43546178d5ed8c375
marlisonmourao <bentesmourao@gmail.com>
2023-03-15 00:15:28 -0400

    create Home screen

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,30 @@
+import { FiPlus } from 'react-icons/fi'
+import { CardInfoFilme } from '../../components/CardInfoFilm'
 import { Header } from '../../components/Header'
-import { Container } from './styles'
+import {
+  Container,
+  ButtonCreate,
+  Content,
+  ContainerButtonCreate,
+} from './styles'
 
 export function Home() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <ContainerButtonCreate>
+          <h1>Meus filmes</h1>
+          <ButtonCreate>
+            <FiPlus />
+            Adicionar filme
+          </ButtonCreate>
+        </ContainerButtonCreate>
+
+        <CardInfoFilme />
+        <CardInfoFilme />
+      </Content>
     </Container>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -10,3 +10,33 @@ export const Container = styled.header`
   grid-template-rows: 11.6rem auto;
   grid-template-areas: 'header' 'content';
 `
+
+export const Content = styled.main`
+  grid-area: content;
+`
+
+export const ContainerButtonCreate = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 4rem;
+
+  > h1 {
+    font-size: 3.2rem;
+    font-weight: normal;
+    line-height: 4.2rem;
+    color: ${({ theme }) => theme.white};
+  }
+`
+
+export const ButtonCreate = styled.button`
+  border: none;
+  padding: 1.35rem 1rem;
+  background: ${({ theme }) => theme['pink-700']};
+  border-radius: 0.8rem;
+
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+`
